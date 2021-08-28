b8d83724099555e55ff1c90510cddf31d2df57b3
bohai <bohai@charlsdata.com>
2021-08-28 20:48:46 +0800

    test features implementation

--- server/tests/unit/test_features.py
+++ server/tests/unit/test_features.py
@@ -1,23 +1,33 @@
+from typing import Optional
+from pygls.lsp.types.basic_structures import Position, TextDocumentIdentifier
+from pygls.lsp.types.language_features import HoverParams, DefinitionParams
 
 import pytest
 from mock import Mock
-from pygls.types import (DidCloseTextDocumentParams, DidOpenTextDocumentParams,
-                         TextDocumentIdentifier, TextDocumentItem)
+from pygls.lsp.types import TextDocumentIdentifier
 from pygls.workspace import Document, Workspace
 
-from ...server import completions
+from ...server import completions, hover, goto_definition
 
 
 class FakeServer():
-    """We don't need real server to unit test features."""
+    """Create fake server to unit test features."""
+    publish_diagnostics = None
+    show_message = None
+    show_message_log = None
 
     def __init__(self):
         self.workspace = Workspace('', None)
 
 
-fake_document_uri = 'file://fake_stata_code.txt'
-fake_document_content = 'summarize female,detail // This is comment\nreplace female= 1\n'
+fake_document_uri = 'file://fake_dofile.do'
+fake_document_content = 'gen x = 3\nreplace x = 10\nsort x'
+fake_doc_identifier = TextDocumentIdentifier(uri=fake_document_uri)
 fake_document = Document(fake_document_uri, fake_document_content)
+fake_hoverParams = HoverParams(text_document=fake_doc_identifier,
+                               position=Position(line=2, character=1))
+fake_defParams = DefinitionParams(text_document=fake_doc_identifier,
+                                  position=Position(line=1, character=8))
 
 
 server = FakeServer()
@@ -37,10 +47,18 @@ def test_completions():
     completion_list = completions()
     labels = [i.label for i in completion_list.items]
 
-    assert '"' in labels
-    assert '[' in labels
-    assert ']' in labels
-    assert '{' in labels
-    assert '}' in labels
+    assert 'nlist' in labels
+    assert 'nlogit' in labels
+    assert 'notes' in labels
 
 
+def test_hover(server, fake_hoverParams):
+    result = hover(server, fake_hoverParams)
+    docstring = result.contents
+    assert '## Syntax\n\n`sort`' in docstring
+
+
+def test_goto_definition(server, fake_defParams):
+    result = goto_definition(server, fake_defParams)
+    assert 0 == result.range.start.line
+    assert 4 == result.range.start.character
